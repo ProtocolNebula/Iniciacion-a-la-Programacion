e7de76832d7f776ebf7cc4e3e469aa68b7a34533
ProtocolNebula <powergm@hotmail.es>
2019-01-04 19:21:24 +0100

    Añadido ejercicio de variables

---
+++ Ejercicios/1-Variables-Vectores/4-Variables-Texto.js
@@ -0,0 +1,22 @@
+var nombreCocinero = 'Rubén';
+
+console.log(nombreCocinero);
+console.log('¡Bienvenido de nuevo ' + nombreCocinero + '!');
+
+var huevos = 0;
+console.log('Actualmente tienes ' + huevos + ' huevos.');
+
+huevos += 5;
+console.log('Ahora tienes ' + huevos + ' huevos.');
+
+console.log('Vamos a preparar un huevo frito!');
+console.log('Preparando sartén.');
+console.log('Encendiendo el fuego.');
+console.log('Echamos aceite.');
+
+console.log('Echamos el huevo.');
+huevos -= 1;
+console.log('Ahora tienes ' + huevos + ' huevos.');
+
+console.log('Esperando a que esté cocinado.');
+console.log('Sacando el huevo de la sartén.');
